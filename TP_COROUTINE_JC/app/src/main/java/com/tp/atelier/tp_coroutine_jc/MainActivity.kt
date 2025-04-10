dda1c7a5ece117fa8f504ff7771655f84fe947f9
Souhail Bektachi <souhalshiv@gmail.com>
2025-04-10 17:40:16 +0100

    feat: Initialize TP_COROUTINE_JC project with basic structure and theming
    - Added Color.kt for color definitions used in the app theme.
    - Created Theme.kt to define light and dark color schemes and apply MaterialTheme.
    - Introduced Type.kt for typography styles in the app.
    - Added vector drawables for launcher icons (foreground and background).
    - Created mipmap resources for different screen densities.
    - Defined colors and strings in XML resources.
    - Set up themes in themes.xml.
    - Included backup and data extraction rules XML files.
    - Added ExampleUnitTest for basic unit testing.
    - Configured build.gradle.kts for project-level settings and dependencies.
    - Established gradle.properties for project-wide Gradle settings.
    - Created libs.versions.toml for dependency version management.
    - Added gradle wrapper files for Gradle version control.
    - Configured settings.gradle.kts for project structure and repositories.

---
+++ TP_COROUTINE_JC/app/src/main/java/com/tp/atelier/tp_coroutine_jc/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.tp.atelier.tp_coroutine_jc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tp.atelier.tp_coroutine_jc.ui.theme.TP_COROUTINE_JCTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TP_COROUTINE_JCTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TP_COROUTINE_JCTheme {
+        Greeting("Android")
+    }
+}
